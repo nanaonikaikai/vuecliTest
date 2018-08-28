c67b4141e28d2b199abf9ad86ffc2437cec13ccf
kaikai <m13339490373@163.com>
2018-08-28 16:26:37 +0800

    0828

--- vuecliTest/src/App.vue
+++ vuecliTest/src/App.vue
@@ -17,7 +17,6 @@
   <!-- <router-view ></router-view>
  <router-view name="left" style="float:left;width:50%;background-color:#ccc;height:300px;"></router-view>
  <router-view name="right" style="float:right;width:50%;background-color:#c0c;height:300px;"></router-view>
-  
   <router-link to="/params/198/jspang website is very good">params</router-link>
    -->
   
